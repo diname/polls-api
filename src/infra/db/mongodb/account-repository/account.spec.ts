27c62b6aea8ff0e3a5d4f050348bfd52f58e3e64
Lucas Medina <lucasmedinarmc@gmail.com>
2024-03-12 19:21:11 -0300

    fix: fix ts-ignore

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -2,6 +2,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
+  // @ts-ignore
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
 
   afterAll(async () => await MongoHelper.disconnect())
